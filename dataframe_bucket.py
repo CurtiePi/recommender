6fd3b46d6126bd43198684cb25f0039944f913ef
CurtiePi <curtisbklyn@gmail.com>
2020-06-29 13:43:45 -0400

    Renamed the DataframeBucket class file name

--- dataframe_bucket.py
+++ dataframe_bucket.py
@@ -5,13 +5,16 @@ import numpy as np
 
 class DataFrameBucket():
 
-    """Formats data into a dataframe for use by model
+    """
+
+        Formats data into a dataframe for use by model
+
         file_name (str): Name of file containing data
     """
 
     def __init__(self, file_name):
-        self._base_dataframe = pd.read_csv(file_name)
-        self._working_dataframe = self._base_dataframe.copy(True)
+        self._dataframe = pd.read_csv(file_name)
+        self._map = {}
 
 
     def set_required_columns(self, required_cols):
@@ -19,24 +22,29 @@ class DataFrameBucket():
             
             Maniputlate dataframe to only have required columns
 
-            required_cols (list): names of columns that are required for dataframe
+            required_cols (list): names of columns required for dataframe
         """
 
-        colunms = self.working_dataframe.columns
+        colunms = self._dataframe.columns
         drop_cols = [col_name for col_name in columns if col_name not in required_cols]
-        self._working_dataframe = self._working_dataframe.drop(drop_cols, axis=1)
+        self._dataframe = self._dataframe.drop(drop_cols, axis=1)
 
 
-    def convert_category_columns(self, category_cols):
+    def convert_category_columns(self, category_cols, map_cols):
         """
             
             Convert dataframe columns to type category
 
             category_cols (list): names of columns that should be type category
+ 
+            map_cols(list): names of columns that need to map back to original value
         """
 
         convert_dict = {col_name: 'category for col_name in category_cols}
-        self._working_dataframe = self._working_dataframe.astype(convert_dict)
+        self._dataframe = self._dataframe.astype(convert_dict)
+
+        for col_name in map_cols:
+            self._map[col_name] = dict(enumerate(self._dataframe[col_name].cat.categories))
 
 
     def set_tartget_column(self, target_col):
@@ -47,13 +55,11 @@ class DataFrameBucket():
             target_col (str): name of the column to set as the target column
         """
 
-        self._working_dataframe['target'] = self._working_dataframe[target_col].cat.codes
+        self._dataframe['target'] = self._dataframe[target_col].cat.codes
 
 
-        map = dict( enumerate(w_df['CLASS_ONE'].cat.categories))
 
-
-    def normalize_dataframe(self, normalize_cols):
+    def get_normalized_dataframe(self, normalize_cols):
         """
 
             Normalizes the dataframe
@@ -61,32 +67,25 @@ class DataFrameBucket():
             normalize_cols (list): name of the columns on which to normalize
         """
 
+        return self.normalize(self._dataframe, normalize_cols)
     
-
-        new_df = normalize(w_df.drop('target', axis=1).append(new_data_dict, ignore_index=True).tail(), normalize_cols)
-
-        new_df = new_df.iloc[-1]
-
-        norm_df = normalize(w_df, normalize_cols)
-
-        return norm_df, new_df, map
-
 
     def format_input_data(self, input_data, data_cols):
         """
 
-            Incorporate new data into a dataframe format 
+            Incorporate new data into a normalized dataframe format 
 
             input_data (list): list of data that serves as input data
+
             data_cols (list): name of the columns that serve as input
         """
 
         new_data_dict = {data_cols[n]: input_data[n] for n in range(0, len(input_data))}
-        new_df = self._working_dataframe.drop('target', axis=1).append(new_data_dict, ignore_index=True).tail()
-        new_norm_dataframe = self.normalize(new_df, data_cols)
-        new_norm_dataframe = new_norm_dataframe.iloc[-1]
+        new_df = self._dataframe.drop('target', axis=1).append(new_data_dict, ignore_index=True).tail()
+        input_dataframe = self.normalize(new_df, data_cols)
+        input_dataframe = input_dataframe.iloc[-1]
 
-        return new_norm_dataframe
+        return input_data_dataframe
 
 
     def drop_columns(self, cols_to_drop):
@@ -96,7 +95,7 @@ class DataFrameBucket():
 
             cols_to_drop (list): names of columns to be dropped from dataframe
         """
-        self._working_dataframe = self._working_dataframe.drop([cols_to_drop], axis=1)
+        self._dataframe = self._dataframe.drop([cols_to_drop], axis=1)
 
 
     def map_category_cols(self, col_name):
@@ -106,18 +105,21 @@ class DataFrameBucket():
 
             col_name (str): Name of the column to be mapped
         """
-        self._map = dict(enumerate(self._working_dataframe[col_name].cat.categories))
+        self._map = dict(enumerate(self._dataframe[col_name].cat.categories))
 
 
     def normalize(self, df, n_cols):
+        """
+
+            Create a map of original values to category values
+
+            df (Dataframe): dataframe object
+
+            n_cols (list): Names of the column to be normalized
+        """
         for col in n_cols:
             max_val = df[col].max()
             min_val = df[col].min()
             df[col] = (df[col] - min_val)/(max_val - min_val)
 
         return df
-
-
-    def get_data_frame(self):
-        return self._dataframe
-
